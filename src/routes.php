3b136eaaf4eaeb6b6e220111c083dd1608c85c31
Đặng Ngọc Nguyên <dnnguyen3152003@gmail.com>
2024-12-14 21:00:55 +0700

    Merge branch 'master' of https://github.com/dnnguyen03/TechStore into DNNguyen

--- src/routes.php
+++ src/routes.php
@@ -22,7 +22,7 @@ $sessionRoute = "seller";
 // $router->addRoute('/\/post\/update\/(\d+)/', [new PostController(), 'update']);
 // $router->addRoute('/\/post\/delete\/(\d+)/', [new PostController(), 'delete']);
 
-if($sessionRoute == "seller") {
+if ($sessionRoute == "seller") {
     $router->addRoute('/\//', [new SelHomeController(), 'index']);
     $router->addRoute('/\/home/', [new SelHomeController(), 'index']);
 
@@ -32,36 +32,26 @@ if($sessionRoute == "seller") {
     $router->addRoute('/\/products\/delete\/(\d+)/', [new SelProductController(), 'delete']);
 
     $router->addRoute('/\/customers/', [new SelCustomerController(), 'index']);
-<<<<<<< HEAD
 
     $router->addRoute('/\/orders/', [new SelOrderController(), 'index']);
-=======
-    $router->addRoute('/\/customers\/detail/', [new SelCustomerController(), 'detail']);
 
-    $router->addRoute('/\/orders/', [new SelOrderController(), 'index']);
-    $router->addRoute('/\/orders\/detail/', [new SelOrderController(), 'detail']);
->>>>>>> eb73ed7d0fc2151f173908f45884c6c667679ba6
-    
     $router->addRoute('/\/shops/', [new SelShopController(), 'index']);
     $router->addRoute('/\/shops\/create/', [new SelShopController(), 'create']);
     $router->addRoute('/\/shops\/update\/(\d+)/', [new SelShopController(), 'update']);
-    
+
     $router->addRoute('/\/chats/', [new SelChatController(), 'index']);
-
-} else if($sessionRoute == "admin") {
+} else if ($sessionRoute == "admin") {
     $router->addRoute('/\//', [new SelHomeController(), 'index']);
     $router->addRoute('/\/home/', [new SelHomeController(), 'index']);
     $router->addRoute('/\/products/', [new SelProductController(), 'index']);
     $router->addRoute('/\/customers/', [new SelCustomerController(), 'index']);
     $router->addRoute('/\/orders/', [new SelOrderController(), 'index']);
-
-} else if($sessionRoute == "customer") {
+} else if ($sessionRoute == "customer") {
     $router->addRoute('/\//', [new SelHomeController(), 'index']);
     $router->addRoute('/\/home/', [new SelHomeController(), 'index']);
     $router->addRoute('/\/products/', [new SelProductController(), 'index']);
     $router->addRoute('/\/customers/', [new SelCustomerController(), 'index']);
     $router->addRoute('/\/orders/', [new SelOrderController(), 'index']);
-    
 } 
 // $router->addRoute('/\/login/', [new SelHomeController(), 'index']);
 // $router->addRoute('/\/register/', [new SelProductController(), 'index']);
